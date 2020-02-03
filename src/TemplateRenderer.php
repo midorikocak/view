9178495793ed231ed3442f8d2fb0a81d01f58ede
midorikocak <mtkocak@gmail.com>
2020-02-03 13:33:10 +0100

    remove unused

--- src/TemplateRenderer.php
+++ src/TemplateRenderer.php
@@ -27,9 +27,7 @@ class TemplateRenderer implements RendererInterface
         }, $templateVars[1]));
 
         $tokenized = preg_replace('/{{ *([a-zA-Z_0-9-]*)? *}}/', '{{$1}}', $templateContents);
-
-        $varLength = count($templateVars);
-
+        
         foreach ($templateVarNames as $toReplace) {
             $tokenized = str_replace('{{' . $toReplace . '}}', $data[$toReplace] ?? '', $tokenized);
         }
